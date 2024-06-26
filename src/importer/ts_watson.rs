9f2cfd2bf5bd08d8b033544fbe06b52b058003bc
Philip Keil <Philip.Keil@hec.de>
2024-06-26 15:44:53 +0200

    add(import): Task to to test with as a string, basic start

--- src/importer/ts_watson.rs
+++ src/importer/ts_watson.rs
@@ -1,72 +1,39 @@
-use std::fs;
+use std::{fs};
 use std::path::Path;
-use serde::{Deserialize, Deserializer};
-use serde_json::Value;
+use anyhow::Error;
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct TdWatsonFrame {
-    timestamp1: i64,
-    timestamp2: i64,
-    status: String,
-    id: String,
-    items: Vec<String>,
-    timestamp3: i64,
+    start_time: Option<i64>,
+    end_time: Option<i64>,
+    project: Option<String>,
+    id: Option<String>,
+    tags: Option<Vec<String>>,
+    timestamp: Option<i64>,
 }
 
-/**
-[
- [
-  1658206800,
-  1658216700,
-  "projb",
-  "7cf346662e1049e2a9717c729cd695d1",
-  [
-   "taga"
-  ],
-  1658225005
- ],
- [
-  1658216700,
-  1658217600,
-  "proja",
-  "45352e42521242f6babebecfc104bad5",
-  [
-   "tagb"
-  ],
-  1658225019
- ],
-**/
-
-fn from_file(path: impl AsRef<Path>) {
+fn from_file(path: impl AsRef<Path>) -> Result<Vec<TdWatsonFrame>, Error> {
     let data = fs::read_to_string(path).expect("Unable to read file");
-    let json_value: Value = serde_json::from_str(&data).expect("JSON was not well-formatted");
+    parse(data)
+}
 
-    // Ausgabe des geparsten Objekts
-    println!("{:#?}", json_value);
-
-    // Deserialisierung der JSON-Werte in die benannte Struktur
-    let parsed: Vec<Vec<TdWatsonFrame>> = json_value.as_array().expect("Expected an array")
-        .iter()
-        .map(|inner_array| {
-            inner_array.as_array().expect("Expected an array of arrays")
-                .iter()
-                .map(|frame| {
-                    serde_json::from_value(frame.clone()).expect("Failed to deserialize frame")
-                })
-                .collect()
-        })
-        .collect();
-
-    // Ausgabe des geparsten Objekts
-    println!("{:#?}", parsed);
+fn parse(content: String) -> Result<Vec<TdWatsonFrame>, Error> {
+    let objects: Vec<TdWatsonFrame> = serde_json::from_str(&content).expect("Failed to parse JSON");
+    Ok(objects)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::importer::ts_watson::{parse};
 
     #[test]
     fn test() {
-        from_file("/Users/kei/Library/Application Support/watson/frames")
+        let data = r#"[
+            [ 1658206800, 1658216700, "projb", "7cf346662e1049e2a9717c729cd695d1", ["taga"], 1658225005 ],
+            [ null, null, "", "", [], null ],
+            [ null, 1658217600, "proja", "45352e42521242f6babebecfc104bad5", ["tagb"], 1658225019 ]
+            ]"#;
+        let result = parse(data.to_string());
     }
 }
